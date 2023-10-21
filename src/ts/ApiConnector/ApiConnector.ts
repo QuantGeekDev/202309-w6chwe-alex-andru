9edc096bb848a217e0d505944cd52131b5d523df
Alex Andru <alex007d@gmail.com>
2023-10-21 19:30:27 +0200

    Add getPokemonList method & test

--- src/ts/ApiConnector/ApiConnector.ts
+++ src/ts/ApiConnector/ApiConnector.ts
@@ -1,10 +1,31 @@
+import { type PokemonListItem, type PokemonListResponse } from "../types/types";
 class ApiConnector {
   baseUrl = "https://pokeapi.co/api/v2/";
   category = "pokemon";
 
-  buildUrl(pokemonId: number): string {
+  buildPokemonUrl(pokemonId: number): string {
     const url = this.baseUrl + this.category + "/" + pokemonId.toString();
     return url;
+  }
+
+  buildPokemonListUrl(
+    offsetFromListStart: number,
+    numberOfPokemonsToDisplay: number,
+  ) {
+    const offsetParam = `offset=${offsetFromListStart}`;
+    const numberOfPokemonsToDisplayParam = `limit=${numberOfPokemonsToDisplay}`;
+
+    const url = `${this.baseUrl}${this.category}?${offsetParam}&${numberOfPokemonsToDisplayParam}`;
+    return url;
+  }
+
+  async getPokemonList(url: string): Promise<PokemonListItem[]> {
+    const response = await fetch(url);
+
+    const responseBody = (await response.json()) as PokemonListResponse;
+    const pokemonlist = responseBody.results;
+
+    return pokemonlist;
   }
 }
 
--- src/ts/types/types.ts
+++ src/ts/types/types.ts
@@ -1,9 +1,17 @@
-interface Pokemon {
+export interface BasePokemon {
   id: number;
   name: string;
   url: string;
   avatarUrl: string;
   types: string[];
 }
-
-export default Pokemon;
+export interface PokemonListResponse {
+  count: number;
+  next: string | undefined;
+  previous: string | undefined;
+  results: PokemonListItem[];
+}
+export interface PokemonListItem {
+  name: string;
+  url: string;
+}
