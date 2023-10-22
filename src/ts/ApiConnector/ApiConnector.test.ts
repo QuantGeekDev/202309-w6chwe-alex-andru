fd0d27d0d78a90d49bdd44987116a0d36938270b
Alex Andru <alex007d@gmail.com>
2023-10-22 15:18:50 +0200

    Modify ApiConnector to work with new Pokemon class

--- src/ts/ApiConnector/ApiConnector.test.ts
+++ src/ts/ApiConnector/ApiConnector.test.ts
@@ -1,4 +1,3 @@
-import { type Pokemon } from "../types/types";
 import ApiConnector from "./ApiConnector";
 
 describe("Given an instance of ApiConnector", () => {
@@ -12,58 +11,57 @@ describe("Given an instance of ApiConnector", () => {
 
       expect(actualUrl).toBe(expectedUrl);
     });
+  });
 
-    describe("When buildPokemonListUrl(0, 20) is called", () => {
-      test("Returns 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=20')", () => {
-        const connector = new ApiConnector();
-        const offsetFromListStart = 0;
-        const numberOfPokemonsToDisplay = 20;
-        const expectedUrl =
-          "https://pokeapi.co/api/v2/pokemon?offset=0&limit=20";
+  describe("When buildPokemonListUrl(0, 20) is called", () => {
+    test("Returns 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=20')", () => {
+      const connector = new ApiConnector();
+      const offsetFromListStart = 0;
+      const numberOfPokemonsToDisplay = 20;
+      const expectedUrl = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=20";
 
-        const actualUrl = connector.buildPokemonListUrl(
-          offsetFromListStart,
-          numberOfPokemonsToDisplay,
-        );
+      const actualUrl = connector.buildPokemonListUrl(
+        offsetFromListStart,
+        numberOfPokemonsToDisplay,
+      );
 
-        expect(actualUrl).toBe(expectedUrl);
-      });
+      expect(actualUrl).toBe(expectedUrl);
     });
+  });
 
-    describe("When getPokemonList('https://pokeapi.co/api/v2/pokemon?offset=0&limit=20') is called)", () => {
-      test("Returns an array of length 20", async () => {
-        const connector = new ApiConnector();
-        const url = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=20";
-        const expectedResponseLength = 20;
-        const pokemonResponse = await connector.getPokemonList(url);
-        const pokemonResponseLength = pokemonResponse.length;
+  describe("When getPokemonList('https://pokeapi.co/api/v2/pokemon?offset=0&limit=20') is called)", () => {
+    test("Returns an array of length 20", async () => {
+      const connector = new ApiConnector();
+      const url = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=20";
+      const expectedResponseLength = 20;
+      const pokemonResponse = await connector.getPokemonList(url);
+      const pokemonResponseLength = pokemonResponse.length;
 
-        expect(pokemonResponseLength).toBe(expectedResponseLength);
-      });
+      expect(pokemonResponseLength).toBe(expectedResponseLength);
     });
+  });
 
-    describe("When getPokemonByUrl('https://pokeapi.co/api/v2/pokemon/2/') is called)", () => {
-      test("Returns object whose 'name' parameter returns 'ivysaur'", async () => {
-        const connector = new ApiConnector();
-        const url = "https://pokeapi.co/api/v2/pokemon/2/";
-        const expectedName = "ivysaur";
-        const pokemon: Pokemon = await connector.getPokemonByUrl(url);
-        const pokemonName = pokemon.name;
+  describe("When getPokemonByUrl('https://pokeapi.co/api/v2/pokemon/2/') is called)", () => {
+    test("Returns object whose 'name' parameter returns 'ivysaur'", async () => {
+      const connector = new ApiConnector();
+      const url = "https://pokeapi.co/api/v2/pokemon/2/";
+      const expectedName = "ivysaur";
+      const pokemon = await connector.getPokemonByUrl(url);
+      const pokemonName = pokemon.getName();
 
-        expect(pokemonName).toBe(expectedName);
-      });
+      expect(pokemonName).toBe(expectedName);
     });
+  });
 
-    describe("When getPokemonById(2) is called)", () => {
-      test("Returns object whose 'name' parameter returns 'ivysaur'", async () => {
-        const connector = new ApiConnector();
-        const ivysaurId = 2;
-        const expectedName = "ivysaur";
-        const pokemon: Pokemon = await connector.getPokemonById(ivysaurId);
-        const pokemonName = pokemon.name;
+  describe("When getPokemonById(2) is called)", () => {
+    test("Returns object whose 'name' parameter returns 'ivysaur'", async () => {
+      const connector = new ApiConnector();
+      const ivysaurId = 2;
+      const expectedName = "ivysaur";
+      const pokemon = await connector.getPokemonById(ivysaurId);
+      const pokemonName = pokemon.getName();
 
-        expect(pokemonName).toBe(expectedName);
-      });
+      expect(pokemonName).toBe(expectedName);
     });
   });
 });
