b1937ace6da83b090e708783018915759a40e3cb
Alex Andru <alex007d@gmail.com>
2023-10-22 19:16:42 +0200

    Fix missing alt text for pokemon__avatar

---
+++ src/ts/index.ts
@@ -0,0 +1,11 @@
+import ApiConnector from "./ApiConnector/ApiConnector.js";
+import PokemonList from "./Components/PokemonList/PokemonList.js";
+
+const app = document.querySelector(".app")!;
+const connector = new ApiConnector();
+const url = connector.buildPokemonListUrl(0, 20);
+const pokemonList = await connector.getPokemonList(url);
+
+const pokemonListComponent = new PokemonList(app, pokemonList);
+
+pokemonListComponent.render();
