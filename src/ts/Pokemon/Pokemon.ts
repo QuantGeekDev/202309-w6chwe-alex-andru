3bda9376ef934131a84748e4e9a96361e24cbcc6
Alex Andru <alex007d@gmail.com>
2023-10-22 04:19:38 +0200

    Add Pokemon class

---
+++ src/ts/Pokemon/Pokemon.ts
@@ -0,0 +1,10 @@
+class Pokemon {
+  constructor(
+    protected id: number,
+    protected name: string,
+    protected url: string,
+    protected avatarUrl: string,
+  ) {}
+}
+
+export default Pokemon;
