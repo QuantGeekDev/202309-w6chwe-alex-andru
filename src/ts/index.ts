b994c9a9b4e8ff6dc5f10a12757e61a44368ccfa
Alex Andru <alex007d@gmail.com>
2023-10-22 22:59:01 +0200

    Add index with rendering logic

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -3,7 +3,7 @@ import PokemonList from "./Components/PokemonList/PokemonList.js";
 
 const app = document.querySelector(".app")!;
 const connector = new ApiConnector();
-const url = connector.buildPokemonListUrl(0, 20);
+const url = connector.buildPokemonListUrl(0, 10);
 const pokemonList = await connector.getPokemonList(url);
 
 const pokemonListComponent = new PokemonList(app, pokemonList);
